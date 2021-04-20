56ec7e99fed5e306b61b2766b8ec39a8421c4c89
Sandi Pradana <sandipradana.id@gmail.com>
2021-04-20 13:01:22 +0700

    String

---
+++ string.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var iniString string = "String"
+
+	fmt.Println(iniString)
+	fmt.Println("Get i : ", string(iniString[3]))
+	fmt.Println("Get lenght : ", len(iniString))
+}
